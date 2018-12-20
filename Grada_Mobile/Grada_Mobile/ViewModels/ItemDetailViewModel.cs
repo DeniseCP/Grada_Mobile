ef4e6aac23f06a14e2402d0dbfbcddbc4f432b5f
Denise <decpereira@hotmail.com>
2018-12-20 12:08:52 -0800

    Add project files.

---
+++ Grada_Mobile/Grada_Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Grada_Mobile.Models;
+
+namespace Grada_Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
